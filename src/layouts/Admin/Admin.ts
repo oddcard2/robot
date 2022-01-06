a2ef35d31c2f2e36745613e7497663f43d545bdf
Oddcard <oddcard2@yandex.ru>
2022-01-06 16:26:47 +0500

    fontawesome and png files removed, embedded svg icons added

--- src/layouts/Admin/Admin.ts
+++ src/layouts/Admin/Admin.ts
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/js/bootstrap.min.js';
-import '@fortawesome/fontawesome-free/css/all.min.css';
+// import '@fortawesome/fontawesome-free/css/all.min.css';
 import { Options, Vue } from 'vue-class-component';
 import SidebarComponent from '@/components/Sidebar/index.vue';
 import NavbarComponent from '@/components/Navbar/index.vue';
